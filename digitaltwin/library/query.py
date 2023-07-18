304c8d68df3e744c28fb5505f227bc0e8483362f
ShenXiaoxue <Xiaoxue.Shen@liverpool.ac.uk>
2023-07-18 14:42:37 +0100

    added the route for /library/query.py in /routes/module1.py, so that it can be found

--- digitaltwin/library/query.py
+++ digitaltwin/library/query.py
@@ -10,18 +10,19 @@ This functions takes the input from the query box on the home page of the app.
 
 from flask import render_template, request
 import os
+from ..digitaltwin import bp
 
-@app.route('/Home', methods=['GET', 'POST'])
+@bp.route('/Home', methods=['GET', 'POST'])
 def Nominal():
     form={'Query':'What is the material  properties of the components?'}
     return render_template('home.html', form=form, date=date)
 
-@app.route('/Home_sub', methods=['GET', 'POST'])
+@bp.route('/home_sub', methods=['GET', 'POST'])
 def Query():
-    req = request.form
-    query = req.get('Query')
+    query = request.form['Query']
     print(query)
-    return render_template('home.html', date=date)
+    #return "Text extracted successfully"
+    return render_template("home_2.html")
 
 '''
 <!--<td><input type="text" id="Query" name="Query" step=any value="{{form['Query']}}"></td> -->
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+source env/bin/activate
 gunicorn --bind=127.0.0.1:5000 "digitaltwin:create_app()"
